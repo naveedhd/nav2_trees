e91bfd94ce1af974d122df78bcb59248b9f421d4
Naveed Usmani <naveedhd@gmail.com>
2019-10-30 23:28:18 +0100

    [simple] pre-emptable goals

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ Behaviour Trees written using [py_trees](https://github.com/splintered-reality/p
 * Run `ros2 run nav2_trees simple`.
 * Localize the robot using `2D Pose Estimate` tool in rviz.
 * Set the goal in rviz using `2D Nav goal` (Set Goal Tool).
+* (Optional) run `py-trees-tree-watcher` or `py-trees-tree-viewer` to visualize the tree
 
+
+![Simple](images/simple.gif "Simple navigation tree")
--- nav2_trees/simple.py
+++ nav2_trees/simple.py
@@ -12,7 +12,7 @@ import py_trees_ros
 from nav2_msgs.action import ComputePathToPose, FollowPath
 import rclpy
 
-from typing import Any, Callable
+import operator
 import sys
 
 from . import actions
@@ -31,16 +31,39 @@ logger = py_trees.logging.Logger("Nosetest")
 ##############################################################################
 
 def create_tree():
-    root = py_trees.composites.Sequence(
-        name='root',
+
+    # abusing selector to work as parallel
+    root = py_trees.composites.Selector(
+        name='root'
     )
 
-    goal2bb = py_trees_ros.subscribers.ToBlackboard(
-        name='Goal2BB',
-        topic_name='/goal',
-        topic_type=PoseStamped,
-        qos_profile=py_trees_ros.utilities.qos_profile_unlatched(),
-        blackboard_variables={'goal': None}
+    # making to always listen to topic
+    goal2bb = py_trees.decorators.RunningIsFailure(
+        child=py_trees_ros.subscribers.ToBlackboard(           
+            topic_name='/goal',
+            topic_type=PoseStamped,
+            qos_profile=py_trees_ros.utilities.qos_profile_unlatched(),
+            blackboard_variables={'goal': None},
+            initialise_variables={'goal': None},
+            name='Goal2BB',
+            clearing_policy=py_trees.common.ClearingPolicy.ON_SUCCESS
+        )
+    )
+
+    rollin = py_trees.composites.Sequence(
+        name="Rollin"
+    )
+
+    # only useful for first time
+    wait_for_goal = py_trees.behaviours.WaitForBlackboardVariableValue(
+        variable_name='goal',
+        expected_value=None,
+        comparison_operator=operator.ne,
+        name="Wait for goal?"
+    )
+
+    navigate_to_pose = py_trees.composites.Sequence(
+        name='Navigate2Pose',
     )
     
     compute_path_to_pose_client = actions.ActionClientToBlackBoard(
@@ -51,7 +74,7 @@ def create_tree():
         blackboard_goal_key='goal',
         blackboard_result_key='path',
         generate_feedback_message=lambda msg: "received feedback"
-    )    
+    )
 
     follow_path_client = actions.ActionClientFromBlackBoard(
         name='Follow Path',
@@ -62,9 +85,21 @@ def create_tree():
         generate_feedback_message=lambda msg: "received feedback"
     )
 
-    root.add_child(goal2bb)
-    root.add_child(compute_path_to_pose_client)
-    root.add_child(follow_path_client)
+    clear_goal = py_trees.behaviours.SetBlackboardVariable(
+        variable_name='goal',
+        variable_value=None,
+        name="Clear goal"
+    )
+
+    root.add_child(goal2bb)    
+    root.add_child(rollin)
+
+    rollin.add_child(wait_for_goal)
+    rollin.add_child(navigate_to_pose)
+
+    navigate_to_pose.add_child(compute_path_to_pose_client)
+    navigate_to_pose.add_child(follow_path_client)
+    navigate_to_pose.add_child(clear_goal)
 
     return root
     
